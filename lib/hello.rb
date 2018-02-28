19b7bb40efddee0e29029f581f68dd89b8a52225
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 22:14:01 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,10 +8,10 @@ def hello_t(array)
     i = i + 1
   end
   array
-else 
+else
   puts "Hey! No block was given!"
 end
-
+end
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
